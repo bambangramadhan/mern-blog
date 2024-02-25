f6df91e76cc6faf70f586e7024a56cfd86332a26
bambangramadhan <bambangramadhan96@gmail.com>
2024-02-25 19:04:34 +0700

    feat: install react typescript & tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-300">app</h1>
+      <h2 className=" text-green-300 ">Hi</h2>
+    </div>
+  );
+};
+
+export default App;
